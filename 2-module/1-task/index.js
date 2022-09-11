504263bdc8c81fd8c2d5f7dd2172ede87200e0c4
Sergei-Shvets <shvets555@mail.ru>
2022-09-11 16:23:18 +0300

    module 2 corrected

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,7 @@
 function sumSalary(salaries) {
   let total = 0;
   for (let number of Object.values(salaries).filter(item => typeof item === "number")) {
-      if (number > 0 && number < Infinity)
+      if (isFinite(number))
       total += number;
   }   
   return total;
