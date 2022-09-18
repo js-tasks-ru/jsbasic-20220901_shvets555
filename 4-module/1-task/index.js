f88f8b94e4a338dabcd1166863fb909bd072f092
Sergei-Shvets <shvets555@mail.ru>
2022-09-18 14:58:56 +0300

    module 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+    for(let name of friends) {
+        const li = document.createElement('li');
+        li.innerHTML = name.firstName + " " + name.lastName;
+        ul.append(li);
+    }
+    return ul;
 }
